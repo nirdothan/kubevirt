67027ebba20e388bc9cc6488784285a6196ce1e1
Nir Dothan <ndothan@redhat.com>
2026-01-18 18:56:48 +0200

    net, netresources: Introduce net resouces package
    In order to centralize all network related
    resouce calculations, introduce a new package.
    Move netbinding memory calculations into this
    package.
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- cmd/virt-handler/virt-handler.go
+++ cmd/virt-handler/virt-handler.go
@@ -36,6 +36,8 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"libvirt.org/go/libvirtxml"
 
+	"kubevirt.io/kubevirt/pkg/network/netresources"
+
 	"kubevirt.io/kubevirt/pkg/virt-handler/ksm"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -68,7 +70,6 @@ import (
 	metrics "kubevirt.io/kubevirt/pkg/monitoring/metrics/virt-handler"
 	metricshandler "kubevirt.io/kubevirt/pkg/monitoring/metrics/virt-handler/handler"
 	"kubevirt.io/kubevirt/pkg/monitoring/profiler"
-	"kubevirt.io/kubevirt/pkg/network/netbinding"
 	"kubevirt.io/kubevirt/pkg/network/passt"
 	netsetup "kubevirt.io/kubevirt/pkg/network/setup"
 	"kubevirt.io/kubevirt/pkg/service"
@@ -407,7 +408,7 @@ func (app *virtHandlerApp) Run() {
 		&capabilities,
 		netConf,
 		netStat,
-		netbinding.MemoryCalculator{},
+		netresources.MemoryCalculator{},
 		passtRepairHandler,
 	)
 	if err != nil {
--- pkg/virt-controller/watch/application.go
+++ pkg/virt-controller/watch/application.go
@@ -37,6 +37,7 @@ import (
 
 	clone "kubevirt.io/api/clone/v1beta1"
 
+	"kubevirt.io/kubevirt/pkg/network/netresources"
 	clonecontroller "kubevirt.io/kubevirt/pkg/virt-controller/watch/clone"
 	"kubevirt.io/kubevirt/pkg/virt-controller/watch/migration"
 	"kubevirt.io/kubevirt/pkg/virt-controller/watch/node"
@@ -695,7 +696,7 @@ func (vca *VirtControllerApp) initCommon() {
 				return hooks.UnmarshalHookSidecarList(vmi)
 			}),
 		services.WithSidecarCreator(netbinding.NetBindingPluginSidecarList),
-		services.WithNetBindingPluginMemoryCalculator(netbinding.MemoryCalculator{}),
+		services.WithNetBindingPluginMemoryCalculator(netresources.MemoryCalculator{}),
 		services.WithAnnotationsGenerators(netAnnotationsGenerator, storageannotations.Generator{}),
 		services.WithNetTargetAnnotationsGenerator(netAnnotationsGenerator),
 	)
